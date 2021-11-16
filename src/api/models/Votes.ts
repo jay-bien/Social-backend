be43a7bb1c88dafab2af3b91a4ece6a9fb94881f
Jay Bien-aimé <jaybien@Jays-MacBook-Air.local>
2021-11-16 18:36:09 -0500

    basic shell of vote model

---
+++ src/api/models/Votes.ts
@@ -0,0 +1,47 @@
+import mongoose, { ObjectId } from 'mongoose';
+import { ObjectID } from "mongodb";
+
+
+
+interface VoteAttrs {
+    author: {
+        type: ObjectID,
+        ref: "User",
+        required: true
+    },
+    commentId: {
+        required: true,
+        type: ObjectID,
+        direction: 1 | -1
+    }
+
+    
+
+    
+}
+
+
+interface VoteDoc extends mongoose.Document{
+   
+}
+
+interface VoteModel extends mongoose.Model< any >{
+
+    build( attrs: VoteAttrs ): VoteDoc
+}
+
+const voteSchema = new mongoose.Schema({
+
+})
+
+voteSchema.statics.build = ( attrs: VoteAttrs ) => {
+    return new Vote( attrs );
+}
+
+const Vote = mongoose.model< VoteDoc, VoteModel >('Vote', voteSchema)
+
+
+
+
+
+export { Vote}
